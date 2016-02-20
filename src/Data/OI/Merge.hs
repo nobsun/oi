a7951865ac51306a1cfde9e7f272e9d7a8e0896d
Nobuo Yamashita <nobsun@sampou.org>
2016-02-20 15:10:21 +0900

    use MVar

--- src/Data/OI/Merge.hs
+++ src/Data/OI/Merge.hs
@@ -1,4 +1,12 @@
 {-# LANGUAGE TypeOperators #-}
+-- |
+-- Module      : Data.OI.Merge
+-- Copyright   : (c) Nobuo Yamashita 2011-2016
+-- License     : BSD3
+-- Author      : Nobuo Yamashita
+-- Maintainer  : nobsun@sampou.org
+-- Stability   : experimental
+--
 module Data.OI.Merge
  ( -- * merge list
   mergeOI
@@ -9,7 +17,6 @@ import Data.OI.Internal
 
 import Control.Concurrent (forkIO)
 import Control.Concurrent.MVar (MVar,newEmptyMVar,newMVar,takeMVar,putMVar)
-import Control.Concurrent.SSem (SSem,new,signal,wait)
 import System.IO.Unsafe (unsafeInterleaveIO)
 
 mergeOI :: [a] -> [a] -> [a] :-> [a]
@@ -18,6 +25,75 @@ mergeOI = (iooi .) . mergeIO
 nmergeOI :: [[a]] -> [a] :-> [a]
 nmergeOI =  iooi . nmergeIO
 
+
+mergeIO :: [a] -> [a] -> IO [a]
+nmergeIO :: [[a]] -> IO [a]
+
+-- $merge
+-- The 'mergeIO' and 'nmergeIO' functions fork one thread for each
+-- input list that concurrently evaluates that list; the results are
+-- merged into a single output list.  
+
+mergeIO ls rs
+ = newEmptyMVar                >>= \ tail_node ->
+   newMVar tail_node           >>= \ tail_list ->
+   newMVar ()                  >>= \ e ->
+   newMVar 2                   >>= \ branches_running ->
+   let
+    buff = (tail_list,e)
+   in
+    forkIO (suckIO branches_running buff ls) >>
+    forkIO (suckIO branches_running buff rs) >>
+    takeMVar tail_node  >>= \ val ->
+    putMVar e ()        >>
+    return val
+
+nmergeIO lss
+ = let
+    len = length lss
+   in
+    newEmptyMVar          >>= \ tail_node ->
+    newMVar tail_node     >>= \ tail_list ->
+    newMVar ()            >>= \ e ->
+    newMVar len           >>= \ branches_running ->
+    let
+     buff = (tail_list,e)
+    in
+     mapM_ (\ x -> forkIO (suckIO branches_running buff x)) lss >>
+     takeMVar tail_node  >>= \ val ->
+     putMVar e ()        >>
+     return val
+
+type Buffer a
+ = (MVar (MVar [a]), MVar ())
+
+suckIO :: MVar Int -> Buffer a -> [a] -> IO ()
+suckIO branches_running buff@(tail_list,e) vs
+ = case vs of
+        [] -> takeMVar branches_running >>= \ val ->
+              if val == 1 then
+                 takeMVar tail_list     >>= \ node ->
+                 putMVar node []        >>
+                 putMVar tail_list node
+              else
+                 putMVar branches_running (val-1)
+        (x:xs) ->
+                takeMVar e                       >>= \ () ->
+                takeMVar tail_list               >>= \ node ->
+                newEmptyMVar                     >>= \ next_node ->
+                unsafeInterleaveIO (
+                        takeMVar next_node  >>= \ y ->
+                        putMVar e ()        >>
+                        return y)                >>= \ next_node_val ->
+                putMVar node (x:next_node_val)   >>
+                putMVar tail_list next_node      >>
+                suckIO branches_running buff xs
+
+
+
+
+
+{-
 -- The origin of following codes came from Control.Concurrent module.
 -- The original codes using Control.Concurrent.QSem were deprecated.
 -- The following codes are made by modifying the original code to use
@@ -85,3 +161,4 @@ suckIO branches_running buff@(tail_list,e) vs
                 putMVar node (x:next_node_val)   >>
                 putMVar tail_list next_node      >>
                 suckIO branches_running buff xs
+-}
